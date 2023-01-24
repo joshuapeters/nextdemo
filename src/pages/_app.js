d47ed7c13ac381e12d68263a5a6ef82f43c56bfc
Josh Peters <jpeters@wistia.com>
2023-01-24 15:36:30 -0500

    initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+import Head from 'next/head';
+import App from 'next/app';
+import { WistiaProvider } from '@wistia/react-embeds';
+
+export default class ExampleApp extends App {
+  static getInitialProps(opts) {
+    const {
+      ctx: { req },
+      router,
+    } = opts;
+    let href;
+
+    if (opts.ctx.req) {
+      ({ href } = new URL(`http://${req.headers.host}${router.asPath}`));
+    } else {
+      href = location.href;
+    }
+
+    return { ...super.getInitialProps(opts), href };
+  }
+
+  render() {
+    const { Component, href, pageProps, wistia = {} } = this.props;
+
+    return (
+      <WistiaProvider context={wistia} href={href}>
+        <Component {...pageProps} />
+      </WistiaProvider>
+    );
+  }
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,47 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class ExampleDocument extends Document {
+  static async getInitialProps(ctx) {
+    const { renderPage } = ctx;
+    const wistia = {};
+
+    ctx.renderPage = ctx.renderPage.bind(ctx, {
+      enhanceApp: (App) => (props) => <App {...props} wistia={wistia} />,
+    });
+
+    const props = await super.getInitialProps(ctx);
+
+    try {
+      const wistiaResult = await wistia.finalize();
+      return { ...props, wistiaResult };
+    } catch (err) {
+      // If getInitialProps rejects, Next.js swallows the error(!) and proceeds
+      // to render(). Since wistia SSR failing shouldn’t block rendering the
+      // page, we’ll just return the original props and account for the
+      // possibility of wistiaResult being undefined in render, but we also want
+      // to log the error because it’s invisible otherwise.
+
+      console.log(err);
+
+      return props;
+    }
+  }
+
+  render() {
+    const { wistiaResult = {} } = this.props;
+
+    if (wistiaResult.updateNextJsTitle) {
+      wistiaResult.updateNextJsTitle(this.props.head);
+    }
+
+    return (
+      <Html>
+        <Head>{wistiaResult.head}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
